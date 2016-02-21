5666f0061bf7a7d029c0d18dda1469eafa854707
Xiaomin Guan <guanxiaomin0932@gmail.com>
2016-02-21 15:27:28 -0800

    - add map boundary - bind marker and table select - remove unnessesary loops - add infowindow close method - fix some minor issues

--- js/script.js
+++ js/script.js
@@ -9,19 +9,24 @@ var locationData = [
 ];
 
 // init map
-var map;
-var service;
-var infowindow;
+var map, service, infowindow;
 
 function initMap() {
-  var pyrmont = {lat: 37.3894, lng: -122.0819};
+  var pyrmont = {lat: 37.3994, lng: -122.0819};
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: pyrmont,
-    zoom: 13
+    zoom: 12
   });
 
-  infowindow = new google.maps.InfoWindow({maxWidth: 200});
+  infowindow = new google.maps.InfoWindow({maxWidth: 400});
+  var bounds = new google.maps.LatLngBounds();
+  locationData.forEach(function(item) {
+    // console.log(item);
+    var position = new google.maps.LatLng(item[1], item[2]);
+    bounds.extend(position);
+  });
+  map.fitBounds(bounds);
 }
 
 // location class with marker and info window
@@ -32,15 +37,29 @@ function Point(name, lat, lng) {
   this.lng = lng;
 
   this.marker = new google.maps.Marker({
-    position: new google.maps.LatLng(lat, lng),
+    position: {lat: this.lat, lng: this.lng},
     title: name,
-    map: map
+    map: map,
+    animation: google.maps.Animation.DROP // marker animetion
   });
 
   google.maps.event.addListener(this.marker, 'click', function() {
+    infowindow.setContent('');
+    infowindow.open(map, this);
     var pos = this.getPosition();
+    // marker animetion
+    if (this.getAnimation() !== null) {
+      this.setAnimation(null);
+    } else {
+      this.setAnimation(google.maps.Animation.BOUNCE);
+      var marker = this;
+      setTimeout(function(){ marker.setAnimation(null); }, 750);
+    }
     getFourSquare(this.title, pos.lat(), pos.lng(), this);
-    
+    // setTimeout(function () { infowindow.close(); }, 5000);
+    google.maps.event.addListener(map, 'mouseout', function() {
+      infowindow.close();
+    });
   });
 }
 
@@ -49,43 +68,45 @@ function getFourSquare(name, lat, lng, marker) {
   var fourSqURL = 'https://api.foursquare.com/v2/venues/search?client_id=KPJVGNXT2YLGWI5BNBBTYM2DJ5INCF5KEUMTYNLTLKBZHASB&client_secret=W2ABQ51JQIYTIXRSO0OHEMWWL13BE5VME2TD5XDBZPXF4N3S&v=20130815&ll=' + lat + ',' +lng;
   $.getJSON(fourSqURL).done(function(data) {
       var item = data.response.venues[0];
-      var infoContent = '<h4>'+ item.name + '</h4>' + 
-        '<p>' + item.contact.formattedPhone +'</p>'+
-        '<p>' + item.stats.checkinsCount +" Checkins" +'</p>'+
-        '<p>' + item.url + '</p>'+
+      var infoContent = '<h4><a href="'+ item.canonicalUrl +'">'+ item.name + '</a></h4>' + 
+        '<p>' +
+          '<img src="images/info_call_ico.png">' + item.contact.formattedPhone +
+          '<img src="images/info_website_ico.png">' + '<a href="' + item.url+ '">' + item.name + '.com' + '</a>' +
+        '</p>' +
+        '<p>' + item.stats.checkinsCount +" Checkins" +'</p>' + 
         '<p>' + item.hereNow.summary +'</p>';
       infowindow.setContent(infoContent);
-      infowindow.open(map, marker);
   }).fail(function(e) {
-    console.log("4sq error");
-    infowindow.setContent("Foursquare data could not be loaded");
-    infowindow.open(map, marker);
+    console.log('4sq error');
+    infowindow.setContent('Foursquare data could not be loaded');
+    
   });
 }
 
 var LocationViewModel = function() {
-  self = this;
+  var self = this;
   var points = [];
-  for (i = 0; i < locationData.length; i++) {
+  for (var i = 0; i < locationData.length; i++) {
     var data = locationData[i];
     points.push(new Point(data[0], data[1], data[2]))
   }
   self.allLocs = ko.observableArray(points);
-  self.query = ko.observable("");
-  
+
+  self.selectPoint = function(item) {
+    // console.log(item);
+    google.maps.event.trigger(item.marker, 'click');
+  }
+
+  self.query = ko.observable('');
   // console.log(self.allLocs());
   self.filtered = ko.computed(function() {
-    self.allLocs().forEach(function(item) {
-      item.marker.setMap(null);
-    });
-    var arr = ko.utils.arrayFilter(self.allLocs(), function(item) {
+    infowindow.close();
+    return ko.utils.arrayFilter(self.allLocs(), function(item) {
       // console.log(self.query());
-       return item.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+      var flag = item.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+      item.marker.setVisible(flag);
+      return flag;
     });
-    arr.forEach(function(item) {
-      item.marker.setMap(map);
-    });
-    return arr;
   });
   // console.log(self.filtered());
 };
